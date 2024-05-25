f6cdadcce02d944e0b0d152f0e0c80c3b2b4d23a
தேசிகன் <136635078+DhesiTheKing@users.noreply.github.com>
2024-05-25 09:08:23 +0530

    Time: 6 ms (32.80%) | Memory: 45.6 MB (54.06%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -4,12 +4,8 @@ class Solution {
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
+        int[] ans=new int[2];
         int size=0;
-        for(Map.Entry<Integer,Integer>mp:map.entrySet()){
-            if(mp.getValue()==1) size++;
-        }
-        int[] ans=new int[size];
-        size=0;
         for(Map.Entry<Integer,Integer>mp:map.entrySet()){
             if(mp.getValue()==1) ans[size++]=mp.getKey();
         }
