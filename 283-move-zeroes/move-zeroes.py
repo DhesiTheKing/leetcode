c38bda26d9fc2351030ecc39c3ce368bb7e01a83
தேசிகன் <136635078+DhesiTheKing@users.noreply.github.com>
2024-07-03 10:19:57 +0530

    Time: 122 ms (38.82%) | Memory: 12.9 MB (53.85%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,16 +1,10 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
         i=0
         j=0
         while(j<len(nums)):
             if(nums[j]!=0):
-                temp=nums[j]
-                nums[j]=nums[i]
-                nums[i]=temp
+                nums[i],nums[j]=nums[j],nums[i]
                 i+=1
                 j+=1
             else: j+=1
